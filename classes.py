2f1e7b336390ad5183e3548081468a7c1aa1a416
Darren <torggaman@hotmail.com>
2017-07-08 19:55:15 -0500

    Starting off

---
+++ classes.py
@@ -0,0 +1,44 @@
+class Baseclass():
+    health_modifier = 0
+    magic_modifier = 0
+    stamina = 0
+    skills = []
+    subclasses = []
+
+
+class Adventurer(Baseclass):
+    health_modifier = 0.5
+    magic_modifier = 0.5
+    stamina_modifier = 0.5
+    skills = ["Adventurer Skills"]
+    subclasses = ["none"]
+
+
+class Mage(Baseclass):
+    magic_modifier = 1
+    stamina_modifier = 0.5
+    skills = ["Mage Skills"]
+    subclasses = ["Wizard"]
+
+
+class Warrior(Baseclass):
+    health_modifier = 1
+    stamina = 0.5
+    skills = ["Warrior Skills"]
+    subclasses = ["Berserker", "Knight"]
+
+
+class Merchant(Baseclass):
+    skills = ["Merchant"]
+    subclasses = ["Blacksmith", "Alchemist"]
+
+
+class Cleric(Baseclass):
+    skills = ["Cleric"]
+    subclasses = ["Preist"]
+
+
+
+class Archer(Baseclass):
+    skills = ["Archer"]
+    subclases = ["Ranger"]
