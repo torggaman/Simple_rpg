0b2734e65edee0b178a60c6d3df6bfcee2cf4958
Darren <torggaman@hotmail.com>
2017-07-09 14:49:26 -0500

    Make is kinda working down you can move around back can't get out

--- map.py
+++ map.py
@@ -1,6 +1,5 @@
 import character
 
-
 class Basemap:
     name = ""
     width = 0
@@ -36,53 +35,51 @@ def moveplayer():
     movement = True
     while movement:
         action = input("What direction?> ")
-        charmove = character.Playercharacter
-        charmovex = charmove.mapxposition
-        charmovey = charmove.mapyposition
-        showmap[charmovey][charmovex] = ''
+        showmap[character.Playercharacter.mapyposition][character.Playercharacter.mapxposition] = ''
+        checking = checkmovement(action, character.Playercharacter.mapyposition,character.Playercharacter.mapxposition)
         if action == "up":
-            if checkmovement(action, charmovey, charmovex):
-                charmovey -= 1
+            if checking:
+                character.Playercharacter.mapyposition -= 1
             else:
                 print("Cannot move that way")
         elif action == "down":
-            if checkmovement(action, charmovey, charmovex):
-                charmovey += 1
+            if checking:
+                character.Playercharacter.mapyposition += 1
             else:
                 print("Cannot move that way")
         elif action == "left":
-            if checkmovement(action, charmovey, charmovex):
-                charmovex -= 1
+            if checking:
+                character.Playercharacter.mapxposition -= 1
             else:
                 print("Cannot move that way")
         elif action == "right":
-            if checkmovement(action, charmovey, charmovex):
-                charmovex += 1
+            if checking:
+                character.Playercharacter.mapxposition += 1
             else:
                 print("Cannot move that way")
         elif action == "actions":
             movement = False
-        else:
+            return
+        elif not checking:
             print("Try again")
             print("You can type: Up, Down, Left, Right, or actions to leave movement")
-        showmap[charmovey][charmovex] = 'P'
-        displaymap(character.Playercharacter.mapname)
+        else:
+            print("try again")
+        showmap[character.Playercharacter.mapyposition][character.Playercharacter.mapxposition] = 'P'
+        displaymap(showmap)
 
 
 def checkmovement(movement, currentpositiony, currentpositionx):
-    if showmap[currentpositiony][currentpositionx] == '':
-        if str(currentpositiony) is "0" and movement is "up":
-            return False
-        elif str(currentpositionx) is "0" and movement is "left":
-            return False
-        elif str(currentpositiony) is str((len(showmap) - 1)) and movement is "down":
-            return False
-        elif str(currentpositionx) is str((len(showmap[currentpositiony]) - 1)) and movement is "right":
-            return False
-        else:
-            return True
-    else:
+    if currentpositiony == 0 and movement is "up":
         return False
+    elif currentpositionx == 0 and movement is "left":
+        return False
+    elif currentpositiony == (len(showmap) - 1) and movement is "down":
+        return False
+    elif currentpositionx == (len(showmap[currentpositiony]) - 1) and movement is "right":
+        return False
+    else:
+        return True
 
 
 def displaymap(mapname):
