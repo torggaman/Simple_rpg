80d19fedd32237f914b445c3a036fc2917473260
Darren <torggaman@hotmail.com>
2017-07-16 12:02:39 -0500

    Added function for character direction (#1)
    Add some variables and functions for setting up the player to be able to
    have a facing direction that can be used later for attacking and other
    things.

--- character.py
+++ character.py
@@ -3,15 +3,11 @@ import classes as c
 
 
 class Basecharacter:
-    strength = 1
-    intelligence = 1
-    dexterity = 1
-    constitution = 1
-    luck = 1
+    stats = {"strength": 1, "intelligence": 1, "dexterity": 1, "constitution": 1, "luck": 1, }
     health = 0
     magic = 0
     stamina = 0
-    dropRate = 1
+    drop_Rate = 1
     questsTaken = []
     questsComplete = []
     level = 1
@@ -39,16 +35,20 @@ class Basecharacter:
         while self.experience >= (self.level * self.requiredexperience):
             self.level += 1
             print("Level: " + str(self.level) + "\n")
-            self.strength = self.strength + (self.level * self.strengthmodifier)
-            self.intelligence = self.intelligence + (self.level * self.intelligencemodifier)
-            self.dexterity = self.dexterity + (self.level * self.dexteritymodifier)
-            self.constitution = self.constitution + (self.level * self.constitutionmodifier)
-            self.luck = self.luck + (self.level * self.luckmodifier)
+            self.stats["strength"] = self.stats["strength"] + (self.level * self.strengthmodifier)
+            self.stats["intelligence"] = self.stats["intelligence"] + (self.level * self.intelligencemodifier)
+            self.stats["dexterity"] = self.stats["dexterity"] + (self.level * self.dexteritymodifier)
+            self.stats["constitution"] = self.stats["constitution"] + (self.level * self.constitutionmodifier)
+            self.stats["luck"] = self.stats["luck"] + (self.level * self.luckmodifier)
             print("Strength: %d \n"
                   "Intelligence: %d \n"
                   "Dexterity: %d \n"
                   "Constitution: %d \n"
-                  "Luck: %d" % (self.strength, self.intelligence, self.dexterity, self.constitution, self.luck))
+                  "Luck: %d" % (self.stats["strength"],
+                                self.stats["intelligence"],
+                                self.stats["dexterity"],
+                                self.stats["constitution"],
+                                self.stats["luck"]))
 
     def choosestats(self):
         statpoints = 30
@@ -59,56 +59,56 @@ class Basecharacter:
                   "Intelligence: %d \n "
                   "Dexterity: %d \n "
                   "Constitution: %d \n "
-                  "Luck: %d" % (self.strength,
-                                self.intelligence,
-                                self.dexterity,
-                                self.constitution,
-                                self.luck))
+                  "Luck: %d" % (self.stats["strength"],
+                                self.stats["intelligence"], 
+                                self.stats["dexterity"], 
+                                self.stats["constitution"], 
+                                self.stats["luck"]))
             answer = input("Choose a stat")
             if answer == "strength":
                 answer = input("How many points? ")
                 if int(answer) < 11 & int(answer) > statpoints & int(answer) > 0:
-                    self.strength = int(answer)
+                    self.stats["strength"] = int(answer)
             elif answer == "intelligence":
                 answer = input("How many points? ")
                 if int(answer) < 11 & int(answer) > statpoints & int(answer) > 0:
-                    self.intelligence = int(answer)
+                    self.stats["intelligence"] = int(answer)
             elif answer == "dexterity":
                 answer = input("How many points? ")
                 if int(answer) < 11 & int(answer) > statpoints & int(answer) > 0:
-                    self.dexterity = int(answer)
+                    self.stats["dexterity"] = int(answer)
             elif answer == "constitution":
                 answer = input("How many points? ")
                 if int(answer) < 11 & int(answer) > statpoints & int(answer) > 0:
-                    self.constitution = int(answer)
+                    self.stats["constitution"] = int(answer)
             elif answer == "luck":
                 answer = input("How many points? ")
                 if int(answer) < 11 & int(answer) > statpoints & int(answer) > 0:
-                    self.luck = int(answer)
+                    self.stats["luck"] = int(answer)
             else:
                 print("Please try again \nMake sure the number is larger than 0 and no higher than 10")
 
-    def createcharacter(self):
+    def create_character(self):
         self.name = input("What is your name? ")
         print("Welcome: %s" % self.name)
         print("Generating Stats")
-        self.getstats()
+        self.get_stats()
         self.chooseclass()
-        self.gethealth()
-        self.getmagic()
-        self.stamina = 100 + (self.dexterity * 2)
+        self.get_health()
+        self.get_magic()
+        self.stamina = 100 + (self.stats["dexterity"] * 2)
 
-    def gethealth(self):
-        basehealth = 100
-        modifiedhealth = (self.constitution * 10)
-        self.health = (basehealth + modifiedhealth) * self.healthmodifier
+    def get_health(self):
+        base_health = 100
+        modified_health = (self.stats["constitution"] * 10)
+        self.health = (base_health + modified_health) * self.healthmodifier
 
-    def getmagic(self):
+    def get_magic(self):
         basemagic = 50
-        modifiedmagic = (self.intelligence * 5)
+        modifiedmagic = (self.stats["intelligence"] * 5)
         self.health = (basemagic + modifiedmagic) * self.magicmodifier
 
-    def getstats(self):
+    def get_stats(self):
         stats = {"strength": 0, "intelligence": 0, "dexterity": 0, "constitution": 0, "luck": 0}
         for stat in stats:
             stats[stat] = self.generatestats()
@@ -122,11 +122,11 @@ class Basecharacter:
                             stats["dexterity"],
                             stats["constitution"],
                             stats["luck"]))
-        self.strength = stats["strength"]
-        self.intelligence = stats["intelligence"]
-        self.dexterity = stats["dexterity"]
-        self.constitution = stats["constitution"]
-        self.luck = stats["luck"]
+        self.stats["strength"] = stats["strength"]
+        self.stats["intelligence"] = stats["intelligence"]
+        self.stats["dexterity"] = stats["dexterity"]
+        self.stats["constitution"] = stats["constitution"]
+        self.stats["luck"] = stats["luck"]
 
     def generatestats(self):
         minimum = 1
@@ -181,10 +181,10 @@ class Basecharacter:
                               self.health,
                               self.magic,
                               self.stamina,
-                              self.strength,
-                              self.intelligence,
-                              self.dexterity,
-                              self.constitution,
-                              self.luck))
+                              self.stats["strength"],
+                              self.stats["intelligence"], 
+                              self.stats["dexterity"], 
+                              self.stats["constitution"], 
+                              self.stats["luck"]))
 
 Playercharacter = Basecharacter()
